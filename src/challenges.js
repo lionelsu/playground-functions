bb7a4eb03dc5128b970ea39e7f47b3f1f2b2ef1d
Lionel Miranda <bibliothecalionel@gmail.com>
2023-01-30 14:15:10 -0400

    req: compareTrue done

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,10 @@
 // Desafio 1 - Crie a função compareTrue
-
+function compareTrue(param1, param2) {
+  if (param1 && param2) {
+    return true;
+  }
+  return false;
+}
 // Desafio 2 - Crie a função splitSentence
 
 // Desafio 3 - Crie a função concatName
@@ -20,17 +25,20 @@
 
 // Não modifique essas linhas
 module.exports = {
-  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
-  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
-  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
-  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
-  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
-  concatName: typeof concatName === 'function' ? concatName : (() => {}),
-  decode: typeof decode === 'function' ? decode : (() => {}),
-  encode: typeof encode === 'function' ? encode : (() => {}),
-  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
-  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
-  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
-  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
-  techList: typeof techList === 'function' ? techList : (() => {}),
+  calcTriangleArea:
+    typeof calcTriangleArea === 'function' ? calcTriangleArea : () => {},
+  calcRectangleArea:
+    typeof calcRectangleArea === 'function' ? calcRectangleArea : () => {},
+  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : () => {},
+  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : () => {},
+  compareTrue: typeof compareTrue === 'function' ? compareTrue : () => {},
+  concatName: typeof concatName === 'function' ? concatName : () => {},
+  decode: typeof decode === 'function' ? decode : () => {},
+  encode: typeof encode === 'function' ? encode : () => {},
+  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : () => {},
+  footballPoints:
+    typeof footballPoints === 'function' ? footballPoints : () => {},
+  highestCount: typeof highestCount === 'function' ? highestCount : () => {},
+  splitSentence: typeof splitSentence === 'function' ? splitSentence : () => {},
+  techList: typeof techList === 'function' ? techList : () => {},
 };
